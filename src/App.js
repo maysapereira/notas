0bf35d865ba81b60e3e4637a1e7712981a196337
Maysa Pereira <maysalvespereira@gmail.com>
2022-04-18 00:26:01 -0300

    :sparkles: (build) cria esqueleto do projeto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import React from "react"
+
 function App() {
   return (
-    <h1> Iniciando React</h1>
+    <>
+    <form>
+        <input type="text" placeholder="Título" />
+        <textarea placeholder="Escreva sua nota"></textarea>
+        <button>Criar nota</button>
+      </form><ul>
+          <li>
+            <section>
+              <header>
+                <h3>Título</h3>
+              </header>
+              <p>Escreva sua nota</p>
+            </section>
+          </li>
+        </ul>
+      </>
+
   );
 }
 
