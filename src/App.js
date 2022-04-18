c63b9d848d89c6d925611612aac6dddafca6d0f8
Maysa Pereira <maysalvespereira@gmail.com>
2022-04-18 02:15:05 -0300

    :package: (build) organiza componentes em pastas e estiliza

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react"
 import FormularioCadastro from "./componentes/FormularioCadastro";
 import ListaDeNotas from "./componentes/ListaDeNotas";
+import "./assets/App.css"
+import "./assets/index.css";
 
 class App extends Component {
 
   render() {
     return (
       <>
+      <section className="conteudo">
         <FormularioCadastro/>
         <ListaDeNotas/>
+        </section>
         </>
   
     );
