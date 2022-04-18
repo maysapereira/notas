eb4d4902342ded651e63e97e2b37c72e9cb57c17
Maysa Pereira <maysalvespereira@gmail.com>
2022-04-18 01:08:20 -0300

    :sparkles: (refactor) refatora card de notas

--- src/componentes/ListaDeNotas.jsx
+++ src/componentes/ListaDeNotas.jsx
@@ -7,15 +7,18 @@ class ListaDeNotas extends Component {
         return(
 
             <ul>
-                <li>
-                    <CardNota/>
-                </li>
-                <li>
-                    <CardNota/>
-                </li>
-                <li>
-                    <CardNota/>
-                </li>
+                {
+                    Array.of("Trabalho", "Trabalho", "Estudos").map(
+                        categoria => {
+
+                        return(
+                            <li>
+                                <div>{categoria}</div>
+                                <CardNota/>
+                            </li>
+                        )
+                    })
+                }
             </ul>
         )
     }
